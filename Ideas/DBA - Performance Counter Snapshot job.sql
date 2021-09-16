d2af94f3f16ab2e377dd8a89aadd2b0d3bbc05d5
Mark Boomaars <mboomaars@gmail.com>
2021-09-16 08:10:43 +0200

    .

--- Ideas/DBA - Performance Counter Snapshot job.sql
+++ Ideas/DBA - Performance Counter Snapshot job.sql
@@ -1,11 +1,11 @@
 USE [msdb]
 GO
 
-/****** Object:  Job [DBA - Performance Counter Snapshot]    Script Date: 13-9-2021 11:27:58 ******/
+/****** Object:  Job [DBA - Performance Counter Snapshot]    Script Date: 14-9-2021 08:43:48 ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 13-9-2021 11:27:58 ******/
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 14-9-2021 08:43:48 ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
@@ -25,7 +25,7 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DBA - Performance Counter Sn
 		@category_name=N'[Uncategorized (Local)]', 
 		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Performance Counter Snapshot]    Script Date: 13-9-2021 11:27:58 ******/
+/****** Object:  Step [Performance Counter Snapshot]    Script Date: 14-9-2021 08:43:49 ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Performance Counter Snapshot', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
@@ -167,16 +167,17 @@ EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Every 5 minutes', 
 		@enabled=1, 
-		@freq_type=8, 
+		@freq_type=4, 
 		@freq_interval=1, 
 		@freq_subday_type=4, 
 		@freq_subday_interval=5, 
 		@freq_relative_interval=0, 
-		@freq_recurrence_factor=1, 
+		@freq_recurrence_factor=0, 
 		@active_start_date=20210910, 
 		@active_end_date=99991231, 
 		@active_start_time=0, 
-		@active_end_time=235959
+		@active_end_time=235959, 
+		@schedule_uid=N'173e1c5b-a01d-4a03-adc4-63073f86b180'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
