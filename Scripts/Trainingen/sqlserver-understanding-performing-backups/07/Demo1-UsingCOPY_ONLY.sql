30661d67e9f2709476a8b449c89a143b7751843a
Mark Boomaars <mboomaars@gmail.com>
2021-10-02 09:19:28 +0200

    .

--- Scripts/Trainingen/sqlserver-understanding-performing-backups/07/Demo1-UsingCOPY_ONLY.sql
+++ Scripts/Trainingen/sqlserver-understanding-performing-backups/07/Demo1-UsingCOPY_ONLY.sql
@@ -1,28 +1,25 @@
--- Demo script for Using COPY_ONLY demo
+RAISERROR(N'Oops! No, don''t just hit F5. Run these demos one at a time.', 20, 1) WITH LOG;
+GO
 
-USE [master];
+USE master;
 GO
 
 IF DATABASEPROPERTYEX (N'Company', N'Version') > 0
 BEGIN
-	ALTER DATABASE [Company] SET SINGLE_USER
-		WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE [Company];
-END
+    ALTER DATABASE Company SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE Company;
+END;
 GO
 
 -- Create the database
-CREATE DATABASE [Company] ON PRIMARY (
-    NAME = N'Company',
-    FILENAME = N'D:\Pluralsight\Company.mdf')
-LOG ON (
-    NAME = N'Company_log',
-    FILENAME = N'D:\Pluralsight\Company_log.ldf');
+CREATE DATABASE Company
+ON PRIMARY (NAME = N'Company', FILENAME = N'D:\SQLData\Company.mdf')
+LOG ON (NAME = N'Company_log', FILENAME = N'D:\SQLLogs\Company_log.ldf');
 GO
 
 -- Delete everything from the backup history table
 -- Do not do this on a production system!
-USE [msdb];
+USE msdb;
 GO
 
 DECLARE @today DATETIME = GETDATE ();
@@ -30,108 +27,100 @@ EXEC sp_delete_backuphistory @oldest_date = @today;
 GO
 
 -- Create a table
-USE [Company];
+USE Company;
 GO
 
-CREATE TABLE [RandomData] (
-	[c1] INT IDENTITY,
-	[c2] VARCHAR (100));
+CREATE TABLE RandomData (c1 INT IDENTITY, c2 VARCHAR(100));
 GO
 
-INSERT INTO [RandomData] VALUES
-	('Initial data: transaction 1');
+INSERT INTO RandomData
+VALUES ('Initial data: transaction 1');
 GO
 
 -- And perform a full backup
-BACKUP DATABASE [Company]
-TO DISK = N'D:\Pluralsight\Company_Full.bak'
-WITH
-	INIT,
-	NAME = N'Company Full';
+BACKUP DATABASE Company
+TO  DISK = N'D:\SQLBackups\Company_Full.bak'
+WITH INIT,
+     NAME = N'Company Full';
 GO
 
 -- Now add some more data and perform a differential backup
-INSERT INTO [RandomData] VALUES
-	('Transaction 2');
-INSERT INTO [RandomData] VALUES
-	('Transaction 3');
+INSERT INTO RandomData
+VALUES ('Transaction 2');
+INSERT INTO RandomData
+VALUES ('Transaction 3');
 GO
 
-BACKUP DATABASE [Company]
-TO DISK = N'D:\Pluralsight\Company_Diff1.bak'
-WITH
-	INIT,
-	DIFFERENTIAL,
-	NAME = N'Company Differential 1';
+BACKUP DATABASE Company
+TO  DISK = N'D:\SQLBackups\Company_Diff1.bak'
+WITH INIT,
+     DIFFERENTIAL,
+     NAME = N'Company Differential 1';
 GO
 
 -- And more data
-INSERT INTO [RandomData] VALUES
-	('Transaction 4');
-INSERT INTO [RandomData] VALUES
-	('Transaction 5');
+INSERT INTO RandomData
+VALUES ('Transaction 4');
+INSERT INTO RandomData
+VALUES ('Transaction 5');
 GO
 
 -- Someone performs an out-of-band full backup
 -- but this time with the COPY_ONLY option
-BACKUP DATABASE [Company]
-TO DISK = N'D:\Pluralsight\Company_Full_For_Dev.bak'
-WITH
-	INIT,
-	NAME = N'Quick full backup for dev',
-	COPY_ONLY;
+BACKUP DATABASE Company
+TO  DISK = N'D:\SQLBackups\Company_Full_For_Dev.bak'
+WITH INIT,
+     NAME = N'Quick full backup for dev',
+     COPY_ONLY;
 GO
 
 -- And perform another differential backup
-BACKUP DATABASE [Company]
-TO DISK = N'D:\Pluralsight\Company_Diff2.bak'
-WITH
-	INIT,
-	DIFFERENTIAL,
-	NAME = N'Company Differential 2';
+BACKUP DATABASE Company
+TO  DISK = N'D:\SQLBackups\Company_Diff2.bak'
+WITH INIT,
+     DIFFERENTIAL,
+     NAME = N'Company Differential 2';
 GO
 
 -- Simulate corruption that destroys the database
-USE [master];
+USE master;
 GO
 
-DROP DATABASE [Company];
+DROP DATABASE Company;
 GO
 
 -- We have a full backup and two differentials - so latest differential
 -- Restore the full backup
-RESTORE DATABASE [Company]
-FROM DISK = N'D:\Pluralsight\Company_Full.bak'
-WITH REPLACE, NORECOVERY;
+RESTORE DATABASE Company
+FROM DISK = N'D:\SQLBackups\Company_Full.bak'
+WITH REPLACE,
+     NORECOVERY;
 GO
 
 -- And the differential backup 2
-RESTORE DATABASE [Company]
-FROM DISK = N'D:\Pluralsight\Company_Diff2.bak'
+RESTORE DATABASE Company
+FROM DISK = N'D:\SQLBackups\Company_Diff2.bak'
 WITH NORECOVERY;
 
 -- And finalize the restore sequence
-RESTORE DATABASE [Company] WITH RECOVERY;
+RESTORE DATABASE Company WITH RECOVERY;
 GO
 
 -- More info...
-SELECT 
-	[backup_start_date],
-	(CASE [type]
-		WHEN N'D' THEN N'Full'
-		WHEN N'I' THEN N'Diff'
-		WHEN N'L' THEN N'Log'
-		ELSE N'Unknown'
-	END) AS N'Type',
-	[name],
-	[backup_set_uuid],
-	[differential_base_guid],
-	[first_lsn], 
-	[last_lsn]
-FROM
-	[msdb].[dbo].[backupset]
-WHERE
-	[database_name] = N'Company'
-ORDER BY
-	[backup_start_date];
+SELECT backup_start_date,
+       (CASE type
+            WHEN N'D' THEN N'Full'
+            WHEN N'I' THEN N'Diff'
+            WHEN N'L' THEN N'Log'
+            ELSE N'Unknown'
+        END
+       ) AS "Type",
+       name,
+       backup_set_uuid,
+       differential_base_guid,
+       first_lsn,
+       last_lsn
+FROM msdb.dbo.backupset
+WHERE database_name = N'Company'
+ORDER BY backup_start_date;
 GO
