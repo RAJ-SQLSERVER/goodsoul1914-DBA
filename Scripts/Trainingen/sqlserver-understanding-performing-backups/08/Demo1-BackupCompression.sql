f95df0600dd577713e47a7e7c35ae91ca01ea205
Mark Boomaars <mboomaars@gmail.com>
2021-10-02 19:47:31 +0200

    .

--- Scripts/Trainingen/sqlserver-understanding-performing-backups/08/Demo1-BackupCompression.sql
+++ Scripts/Trainingen/sqlserver-understanding-performing-backups/08/Demo1-BackupCompression.sql
@@ -1,8 +1,6 @@
--- Demo script for Backup Compression demo
-
 -- Delete everything from the backup history table
 -- Do not do this on a production system!
-USE [msdb];
+USE msdb;
 GO
 
 DECLARE @today DATETIME = GETDATE ();
@@ -13,106 +11,98 @@ GO
 -- http://bit.ly/1IHkiSg
 
 -- Uncompressed backup
-BACKUP DATABASE [AdventureWorks2014]
-TO DISK = N'C:\Pluralsight\ADV_Uncompressed.bak'
-WITH
-	INIT,
-	NAME = N'AdventureWorks2014 Uncompressed SSD';
+BACKUP DATABASE AdventureWorks
+TO  DISK = N'D:\SQLBackups\ADV_Uncompressed.bak'
+WITH INIT,
+     NAME = N'AdventureWorks Uncompressed SSD';
 GO
 
 -- Compressed backup
-BACKUP DATABASE [AdventureWorks2014]
-TO DISK = N'C:\Pluralsight\ADV_Compressed.bak'
-WITH
-	INIT,
-	NAME = N'AdventureWorks2014 Compressed SSD',
-	COMPRESSION;
+BACKUP DATABASE AdventureWorks
+TO  DISK = N'D:\SQLBackups\ADV_Compressed.bak'
+WITH INIT,
+     NAME = N'AdventureWorks Compressed SSD',
+     COMPRESSION;
 GO
 
-SELECT
-	DATEDIFF (s, [backup_start_date], [backup_finish_date]) AS [Time],
-	(CASE [type]
-		WHEN N'D' THEN N'Full'
-		WHEN N'I' THEN N'Diff'
-		WHEN N'L' THEN N'Log'
-		ELSE N'Unknown'
-	END) AS N'Type',
-	[name],
-	[backup_size],
-	[compressed_backup_size]
-FROM
-	[msdb].[dbo].[backupset]
-ORDER BY
-	[backup_start_date];
+SELECT DATEDIFF (s, backup_start_date, backup_finish_date) AS "Time",
+       (CASE type
+            WHEN N'D' THEN N'Full'
+            WHEN N'I' THEN N'Diff'
+            WHEN N'L' THEN N'Log'
+            ELSE N'Unknown'
+        END
+       ) AS "Type",
+       name,
+       backup_size,
+       compressed_backup_size
+FROM msdb.dbo.backupset
+ORDER BY backup_start_date;
 GO
 
 -- What about restore?
-DROP DATABASE [AdventureWorks2014];
+DROP DATABASE AdventureWorks;
 GO
 
-RESTORE DATABASE [AdventureWorks2014]
-FROM DISK = N'C:\Pluralsight\ADV_Uncompressed.bak'
+RESTORE DATABASE AdventureWorks
+FROM DISK = N'D:\SQLBackups\ADV_Uncompressed.bak'
 WITH REPLACE;
 GO
 
-DROP DATABASE [AdventureWorks2014];
+DROP DATABASE AdventureWorks;
 GO
 
-RESTORE DATABASE [AdventureWorks2014]
-FROM DISK = N'C:\Pluralsight\ADV_Compressed.bak'
+RESTORE DATABASE AdventureWorks
+FROM DISK = N'D:\SQLBackups\ADV_Compressed.bak'
 WITH REPLACE;
 GO
 
 -- And now with the backup stored on a spinning drive
 
 -- Uncompressed backup
-BACKUP DATABASE [AdventureWorks2014]
-TO DISK = N'I:\Pluralsight\ADV_Uncompressed.bak'
-WITH
-	INIT,
-	NAME = N'AdventureWorks2014 Uncompressed Spinning';
+BACKUP DATABASE AdventureWorks
+TO  DISK = N'D:\SQLBackups\ADV_Uncompressed.bak'
+WITH INIT,
+     NAME = N'AdventureWorks2014 Uncompressed Spinning';
 GO
 
 -- Compressed backup
-BACKUP DATABASE [AdventureWorks2014]
-TO DISK = N'I:\Pluralsight\ADV_Compressed.bak'
-WITH
-	INIT,
-	NAME = N'AdventureWorks2014 Compressed Spinning',
-	COMPRESSION;
+BACKUP DATABASE AdventureWorks
+TO  DISK = N'D:\SQLBackups\ADV_Compressed.bak'
+WITH INIT,
+     NAME = N'AdventureWorks2014 Compressed Spinning',
+     COMPRESSION;
 GO
 
-SELECT
-	DATEDIFF (s, [backup_start_date], [backup_finish_date]) AS [Time],
-	(CASE [type]
-		WHEN N'D' THEN N'Full'
-		WHEN N'I' THEN N'Diff'
-		WHEN N'L' THEN N'Log'
-		ELSE N'Unknown'
-	END) AS N'Type',
-	[name],
-	[backup_size],
-	[compressed_backup_size]
-FROM
-	[msdb].[dbo].[backupset]
-ORDER BY
-	[backup_start_date];
+SELECT DATEDIFF (s, backup_start_date, backup_finish_date) AS "Time",
+       (CASE type
+            WHEN N'D' THEN N'Full'
+            WHEN N'I' THEN N'Diff'
+            WHEN N'L' THEN N'Log'
+            ELSE N'Unknown'
+        END
+       ) AS "Type",
+       name,
+       backup_size,
+       compressed_backup_size
+FROM msdb.dbo.backupset
+ORDER BY backup_start_date;
 GO
 
 -- What about restore?
-DROP DATABASE [AdventureWorks2014];
+DROP DATABASE AdventureWorks;
 GO
 
-RESTORE DATABASE [AdventureWorks2014]
-FROM DISK = N'I:\Pluralsight\ADV_Uncompressed.bak'
+RESTORE DATABASE AdventureWorks
+FROM DISK = N'D:\SQLBackups\ADV_Uncompressed.bak'
 WITH REPLACE;
 GO
 
-DROP DATABASE [AdventureWorks2014];
+DROP DATABASE AdventureWorks;
 GO
 
-RESTORE DATABASE [AdventureWorks2014]
-FROM DISK = N'I:\Pluralsight\ADV_Compressed.bak'
+RESTORE DATABASE AdventureWorks2014
+FROM DISK = N'D:\SQLBackups\ADV_Compressed.bak'
 WITH REPLACE;
 GO
 
