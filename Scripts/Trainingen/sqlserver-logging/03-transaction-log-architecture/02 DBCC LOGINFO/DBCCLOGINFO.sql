87205a4e9bd6eec13f6913237b705159984e2a66
Mark Boomaars <mboomaars@gmail.com>
2021-10-03 17:04:22 +0200

    ,

--- Scripts/Trainingen/sqlserver-logging/03-transaction-log-architecture/02 DBCC LOGINFO/DBCCLOGINFO.sql
+++ Scripts/Trainingen/sqlserver-logging/03-transaction-log-architecture/02 DBCC LOGINFO/DBCCLOGINFO.sql
@@ -1,44 +1,48 @@
-USE [master];
+RAISERROR(N'Oops! No, don''t just hit F5. Run these demos one at a time.', 20, 1) WITH LOG;
+GO
+
+USE master;
 GO
 
 IF DATABASEPROPERTYEX (N'DBMaint2012', N'Version') > 0
 BEGIN
-	ALTER DATABASE [DBMaint2012] SET SINGLE_USER
-		WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE [DBMaint2012];
-END
+    ALTER DATABASE DBMaint2012 SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DBMaint2012;
+END;
 GO
 
 
 -- Create a database
-CREATE DATABASE [DBMaint2012] ON PRIMARY (
-    NAME = N'DBMaint2012_data',
-    FILENAME = N'D:\Pluralsight\DBMaint2012_data.mdf')
+CREATE DATABASE DBMaint2012
+ON PRIMARY (
+       NAME = N'DBMaint2012_data',
+       FILENAME = N'D:\SQLData\DBMaint2012_data.mdf'
+   )
 LOG ON (
     NAME = N'DBMaint2012_log',
-    FILENAME = N'D:\Pluralsight\DBMaint2012_log.ldf',
+    FILENAME = N'D:\SQLLogs\DBMaint2012_log.ldf',
     SIZE = 10MB,
-    FILEGROWTH = 10MB);
+    FILEGROWTH = 10MB
+);
 GO
 
 -- Examine the size of the log
-DBCC SQLPERF (LOGSPACE);
+DBCC SQLPERF(LOGSPACE);
 GO
 
 -- Examine the VLF structure of the log
-DBCC LOGINFO (N'DBMaint2012');
+DBCC LOGINFO(N'DBMaint2012');
 GO
 
 -- Increase the log file size
-ALTER DATABASE [DBMaint2012] MODIFY FILE (
-    NAME = N'DBMaint2012_log',
-    SIZE = 20MB);
+ALTER DATABASE DBMaint2012
+MODIFY FILE (NAME = N'DBMaint2012_log', SIZE = 20MB);
 GO
 
 -- Examine the size of the log
-DBCC SQLPERF (LOGSPACE);
+DBCC SQLPERF(LOGSPACE);
 GO
 
 -- Examine the VLF structure of the log
-DBCC LOGINFO (N'DBMaint2012');
+DBCC LOGINFO(N'DBMaint2012');
 GO
--- Scripts/Trainingen/sqlserver-logging/04-log-records/01 Examining Log Records/LogRecords.sql
+++ Scripts/Trainingen/sqlserver-logging/04-log-records/01 Examining Log Records/LogRecords.sql
@@ -1,18 +1,20 @@
-USE [master];
+RAISERROR(N'Oops! No, don''t just hit F5. Run these demos one at a time.', 20, 1) WITH LOG;
+GO
+
+USE master;
 GO
 
 IF DATABASEPROPERTYEX (N'DBMaint2012', N'Version') > 0
 BEGIN
-	ALTER DATABASE [DBMaint2012] SET SINGLE_USER
-		WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE [DBMaint2012];
-END
+    ALTER DATABASE DBMaint2012 SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DBMaint2012;
+END;
 GO
 
-CREATE DATABASE [DBMaint2012];
+CREATE DATABASE DBMaint2012;
 GO
 
-USE [DBMaint2012];
+USE DBMaint2012;
 GO
 SET NOCOUNT ON;
 GO
@@ -20,14 +22,15 @@ GO
 -- Make sure the database is in SIMPLE
 -- recovery model with no auto-stats (to avoid
 -- unwanted log records)
-ALTER DATABASE [DBMaint2012] SET RECOVERY SIMPLE;
-ALTER DATABASE [DBMaint2012] SET AUTO_CREATE_STATISTICS OFF;
+ALTER DATABASE DBMaint2012 SET RECOVERY SIMPLE;
+ALTER DATABASE DBMaint2012 SET AUTO_CREATE_STATISTICS OFF;
 GO
 
 -- Create a simple table with a record
-CREATE TABLE [test] ([c1] INT, [c2] INT, [c3] INT);
+CREATE TABLE test (c1 INT, c2 INT, c3 INT);
 
-INSERT INTO [test] VALUES (1, 1, 1);
+INSERT INTO test
+VALUES (1, 1, 1);
 GO
 
 -- Clear out the log (more on this in module 5)
@@ -35,36 +38,39 @@ CHECKPOINT;
 GO
 
 -- Implicit transaction to insert a new record
-INSERT INTO [test] VALUES (2, 2, 2);
+INSERT INTO test
+VALUES (2, 2, 2);
 GO
 
 -- Look at various fields, including locks being logged
-SELECT * FROM fn_dblog (NULL, NULL);
+SELECT *
+FROM fn_dblog (NULL, NULL);
 GO
 
 -- Explicit transaction to insert a new record
 BEGIN TRAN;
 GO
-INSERT INTO [test] VALUES (3, 3, 3);
+INSERT INTO test
+VALUES (3, 3, 3);
 GO
 
-SELECT * FROM fn_dblog (NULL, NULL);
+SELECT *
+FROM fn_dblog (NULL, NULL);
 GO
 
 -- Now commit
 COMMIT TRAN;
 GO
 
-SELECT * FROM fn_dblog (NULL, NULL);
+SELECT *
+FROM fn_dblog (NULL, NULL);
 GO
 
 -- Now delete a row
-DELETE FROM [test] WHERE [c1] = 1;
+DELETE FROM test
+WHERE c1 = 1;
 GO
 
-SELECT * FROM fn_dblog (NULL, NULL);
+SELECT *
+FROM fn_dblog (NULL, NULL);
 GO
-
-
-
-	
