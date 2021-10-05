5c9c3635af0fa18ca7017f840055aa3e85aeb189
Mark Boomaars <mboomaars@gmail.com>
2021-10-05 19:31:32 +0200

    Update CircularLog.sql

--- Scripts/Trainingen/sqlserver-logging/06-transaction-log-operations/02 Circular Nature of the Transaction Log/CircularLog.sql
+++ Scripts/Trainingen/sqlserver-logging/06-transaction-log-operations/02 Circular Nature of the Transaction Log/CircularLog.sql
@@ -1,32 +1,34 @@
-USE [master];
+USE master;
 GO
 
 IF DATABASEPROPERTYEX (N'DBMaint2012', N'Version') > 0
 BEGIN
-	ALTER DATABASE [DBMaint2012] SET SINGLE_USER
-		WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE [DBMaint2012];
-END
+    ALTER DATABASE DBMaint2012 SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DBMaint2012;
+END;
 GO
 
-CREATE DATABASE [DBMaint2012] ON PRIMARY (
-    NAME = N'DBMaint2012_data',
-    FILENAME = N'D:\Pluralsight\DBMaint2012_data.mdf')
+CREATE DATABASE DBMaint2012
+ON PRIMARY (
+       NAME = N'DBMaint2012_data',
+       FILENAME = N'D:\SQLData\DBMaint2012_data.mdf'
+   )
 LOG ON (
     NAME = N'DBMaint2012_log',
-    FILENAME = N'D:\Pluralsight\DBMaint2012_log.ldf',
+    FILENAME = N'D:\SQLLogs\DBMaint2012_log.ldf',
     SIZE = 5MB,
-    FILEGROWTH = 1MB);
+    FILEGROWTH = 1MB
+);
 GO
 
-USE [DBMaint2012];
+USE DBMaint2012;
 GO
 SET NOCOUNT ON;
 GO
 
 -- Make sure the database is in SIMPLE
 -- recovery model
-ALTER DATABASE [DBMaint2012] SET RECOVERY SIMPLE;
+ALTER DATABASE DBMaint2012 SET RECOVERY SIMPLE;
 GO
 
 -- What does the log look like?
@@ -36,14 +38,13 @@ GO
 -- Create a table that will grow very
 -- quickly and generate lots of transaction
 -- log
-CREATE TABLE [BigRows] (
-	[c1] INT IDENTITY,
-	[c2] CHAR (8000) DEFAULT 'a');
+CREATE TABLE BigRows (c1 INT IDENTITY, c2 CHAR(8000) DEFAULT 'a');
 GO
 
 -- Insert some rows to fill the first
 -- two VLFs and start on the third
-INSERT INTO [BigRows] DEFAULT VALUES;
+INSERT INTO BigRows
+DEFAULT VALUES;
 GO 300
 
 -- What does the log look like now?
@@ -52,8 +53,9 @@ GO
 
 -- Now start an explicit transaction which
 -- will hold VLF 3 and onwards active
-BEGIN TRAN
-INSERT INTO [BigRows] DEFAULT VALUES;
+BEGIN TRAN;
+INSERT INTO BigRows
+DEFAULT VALUES;
 GO
 
 -- Now checkpoint to clear the first two
@@ -66,7 +68,8 @@ GO
 
 -- Now add some more rows that will fill
 -- up VLFs 3 and 4 and then wrap around
-INSERT INTO [BigRows] DEFAULT VALUES;
+INSERT INTO BigRows
+DEFAULT VALUES;
 GO 300 
 
 DBCC LOGINFO;
@@ -75,7 +78,8 @@ GO
 -- Now add some more rows - the log is
 -- forced to grow. What do the VLF
 -- sequence numbers look like?
-INSERT INTO [BigRows] DEFAULT VALUES;
+INSERT INTO BigRows
+DEFAULT VALUES;
 GO 300 
 
 DBCC LOGINFO;
@@ -89,7 +93,7 @@ DBCC LOGINFO;
 GO
 
 -- Look at the amount of log used
-DBCC SQLPERF (LOGSPACE);
+DBCC SQLPERF(LOGSPACE);
 GO
 
 -- How about now?
@@ -101,7 +105,7 @@ GO
 
 -- Look at the amount of log used
 -- What happened?
-DBCC SQLPERF (LOGSPACE);
+DBCC SQLPERF(LOGSPACE);
 GO
 
 -- How about now?
